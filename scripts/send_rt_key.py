ec355bf749d1ae8fc03ba4313ba7dc45bd0f66b9
Emanuel Henriques <emanuel.h200@gmail.com>
2020-05-17 19:41:56 +0100

    Added comments

--- scripts/send_rt_key.py
+++ scripts/send_rt_key.py
@@ -10,17 +10,22 @@ Copy ip addresses from txt file and send ssh commands
   to the remote machines. 
 '''
 
+# Fetch the ip list
 ip_file = open("ip.txt")
 ip_list = ip_file.read().splitlines()
 ip_file.close()
 
 for ip in ip_list:
+    # Build the ssh command
     ssh_command = "ssh admin@{}".format(ip)
+    # Run ping command to check host is alive
     ping = subprocess.run(['ping', '-c 2', ip], stdout=subprocess.PIPE)
     output_ping = ping.stdout.decode("utf-8")
+    # Prints an error if ping fails
     if(output_ping.find("100% packet loss")):
         print("ERRO - IP {} nao esta disponivel!".format(ip))
     else:
+        # Test the command and send the key
         try:
             ssh = pexpect.spawn(ssh_command)
             ssh.expect(' (yes/no)?')
@@ -29,4 +34,5 @@ for ip in ip_list:
             ssh.sendline('quit')
             print("{} OK!".format(ip))
         except:
+            # Print an error if ssh fails
             print("Erro com o IP {}!".format(ip))
