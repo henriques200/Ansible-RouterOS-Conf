f369067faa6ef62195ddf263d3884ba7fe13317a
Emanuel Henriques <emanuel.h200@gmail.com>
2020-06-02 12:53:29 -0400

    Added Ansible Dynamic Inventory Script

---
+++ DB-Scripts/db_to_inventory_original.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import psycopg2
+import json
+
+# Start PostgresSQL connection
+try:
+    connection = psycopg2.connect(user = "postgres",
+                                  password = "123",
+                                  host = "127.0.0.1",
+                                  port = "5432",
+                                  database = "mikrotik")
+
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
+
+
+def get_hosts():
+    hosts = []
+    cursor = connection.cursor()
+    # All IPs from a table
+    if(code == "1"):
+        sql_query = ("SELECT ip_nome FROM routers")
+        cursor.execute(sql_query, )
+    elif(code == "2"):
+        sql_query = ("SELECT ip_nome FROM routers WHERE ip_nome = %s")
+        data = ()
+        cursor.execute(sql_query, data)
+    # Fetch all IP addresses and save to a list
+    record = cursor.fetchall()
+    for result in record:
+        for ip in result:
+            hosts.append(ip)
+    # Creates the inventory dictionary
+    inv_dictionary = {
+    "routeros": {
+      "hosts": hosts,
+      "vars": {
+        "ansible_network_os": "routeros",
+        "ansible_user": "admin",
+        "ansible_password": "",
+        "ansible_connection": "network_cli" 
+      }
+     }
+    }
+    # Converts to JSON format
+    ansible_inventory = json.dumps(inv_dictionary)
+    # print(ansible_inventory)
+    return ansible_inventory
+
+
+if(__name__ == '__main__'):
+    print(get_hosts()) 
